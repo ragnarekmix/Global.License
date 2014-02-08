68b536b2b638c5e6e6431cc8a39d71647c143f26
ragnarekmix <ragnarek.mix@gmail.com>
2014-02-08 16:15:14 +0200

    added logging to smpledata representator

--- src/Global.LicenseManager/Global.LicenseManager.Data/Representators/SimpleDataRepresentator.cs
+++ src/Global.LicenseManager/Global.LicenseManager.Data/Representators/SimpleDataRepresentator.cs
@@ -1,5 +1,6 @@
 ﻿using Global.LicenseManager.Data.Entities;
 using Global.LicenseManager.Data.Interfaces;
+using log4net;
 using Simple.Data;
 using System;
 using System.Collections.Generic;
@@ -11,38 +12,59 @@ namespace Global.LicenseManager.Data.Representators
 {
     public class SimpleDataRepresentator : IDataRepresentator
     {
+        private readonly ILog _log = LogManager.GetLogger(typeof(SimpleDataRepresentator));
+
         public List<Customer> GetAllCustomers()
         {
-            var db = Database.Open();
-            var customers = db.Customers.All().ToList();
             var customerList = new List<Customer>();
-            foreach (var customer in customers)
+            try
             {
-                customerList.Add(new Customer()
+                var db = Database.Open();
+                var customers = db.Customers.All().ToList();
+
+
+                foreach (var customer in customers)
                 {
-                    Id = customer.CustomerId,
-                    FirstName = customer.FirstName,
-                    LastName = customer.LastName
-                });
+                    customerList.Add(new Customer()
+                    {
+                        Id = customer.CustomerId,
+                        FirstName = customer.FirstName,
+                        LastName = customer.LastName
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                _log.ErrorFormat("ERROR: {0}", e.Message);
+                throw new ApplicationException();
             }
             return customerList;
         }
 
         public List<License> GetAllLicenses()
         {
-            var db = Database.Open();
-            var licenses = db.Licenses.All().ToList();
             var licenseList = new List<License>();
-            foreach (var license in licenses)
+            try
             {
-                licenseList.Add(new License()
+                var db = Database.Open();
+                var licenses = db.Licenses.All().ToList();
+
+                foreach (var license in licenses)
                 {
-                    Id = license.LicenseId,
-                    CustomerId = license.CustomerId,
-                    Key = license.Key,
-                    CreationDate = license.CreationDate.ToString("dd MMMM yyyy"),
-                    ModificationDate = license.ModificationDate.ToString("dd MMMM yyyy")
-                });
+                    licenseList.Add(new License()
+                    {
+                        Id = license.LicenseId,
+                        CustomerId = license.CustomerId,
+                        Key = license.Key,
+                        CreationDate = license.CreationDate.ToString("dd MMMM yyyy"),
+                        ModificationDate = license.ModificationDate.ToString("dd MMMM yyyy")
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                _log.ErrorFormat("ERROR: {0}", e.Message);
+                throw new ApplicationException();
             }
             return licenseList;
         }
