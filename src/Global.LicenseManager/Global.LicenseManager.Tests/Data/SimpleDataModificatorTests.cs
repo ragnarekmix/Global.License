da739809a0a693631d3fdcc2116ee157ce1bf05e
Mikhail <ext-mikhail.podobivskyi@lindorff.com>
2014-04-29 14:10:24 +0300

    box for SimpleDataModificatorTests

---
+++ src/Global.LicenseManager/Global.LicenseManager.Tests/Data/SimpleDataModificatorTests.cs
@@ -0,0 +1,30 @@
+﻿using Global.LicenseManager.Common.Interfaces;
+using Global.LicenseManager.Common.Log;
+using Global.LicenseManager.Data.Modificators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Simple.Data;
+
+namespace Global.LicenseManager.Tests.Data
+{
+    [TestClass]
+    public class SimpleDataModificatorTests
+    {
+        IDataModificator sut;
+        Mock<ILogger> log;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            log = new Mock<ILogger>();
+            sut = new SimpleDataModificator(log.Object);
+            var adapter = new InMemoryAdapter();
+            Database.UseMockAdapter(adapter);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
